615f724afcb398e808d1c8c9726bda2a503a8ab5
personal <reddyarun59@yahoo.com>
2024-09-27 09:45:58 +0530

    fix api client

--- src/utils/http-client.ts
+++ src/utils/http-client.ts
@@ -2,19 +2,19 @@ import Axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
 
 class HttpService {
   get<T>(url: string, config: AxiosRequestConfig = {}): Promise<AxiosResponse<T>> {
-    return Axios.get<T>(url, { ...config, withCredentials: true });
+    return Axios.get<T>(url, { ...config });
   }
 
   post<T, D = unknown>(url: string, data: D, config: AxiosRequestConfig = {}): Promise<AxiosResponse<T>> {
-    return Axios.post<T>(url, data, { ...config, withCredentials: true });
+    return Axios.post<T>(url, data, { ...config });
   }
 
   put<T, D = unknown>(url: string, data: D, config: AxiosRequestConfig = {}): Promise<AxiosResponse<T>> {
-    return Axios.put<T>(url, data, { ...config, withCredentials: true });
+    return Axios.put<T>(url, data, { ...config });
   }
 
   delete<T, D = unknown>(url: string, data: D = {} as D, config: AxiosRequestConfig = {}): Promise<AxiosResponse<T>> {
-    return Axios.delete<T>(url, { data, ...config, withCredentials: true });
+    return Axios.delete<T>(url, { data, ...config });
   }
 }
 
